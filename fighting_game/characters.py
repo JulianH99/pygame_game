feac6f140b956e4feec24dcf6095350ec0adc827
Fabio <andresrenzas@gmail.com>
2019-02-20 20:14:38 -0500

    Propuesta sobre otra forma de hacerlo

--- fighting_game/characters.py
+++ fighting_game/characters.py
@@ -1,6 +1,9 @@
-import pygame
+import pygame, glob
 from abc import abstractmethod
 from fighting_game.helpers.colors import *
+
+screen = pygame.display.set_mode([800, 400])
+background = pygame.image.load('./assets/sprites/Backgrounds/BackgroundFairyTail.png').convert()
 
 
 class Character(pygame.sprite.Sprite):
@@ -21,7 +24,7 @@ class Character(pygame.sprite.Sprite):
 
         self.rect.centerx = x
         self.rect.centery = y
-        self.speed = [1, 0.5]
+        self.speed = 3
 
         self.jumping = False
 
@@ -35,34 +38,46 @@ class Character(pygame.sprite.Sprite):
     def handle_keydown(self):
         key = pygame.key.get_pressed()
 
-        if key[pygame.K_RIGHT]:
-            self.rect.centerx += self.speed[0]
-        if key[pygame.K_LEFT]:
-            self.rect.centerx -= self.speed[0]
+        if key[pygame.K_LEFT] and self.x > self.speed:
+            self.rect.centerx -= self.speed
+
+        elif key[pygame.K_RIGHT] and self.x < 800 - self.speed:
+            self.rect.centerx += self.speed
+
         if key[pygame.K_UP]:
             self.jumping = True
 
     def update(self, *args):
         if self.jumping:
             if self.force > 0:
-                F = (0.5 * self.mass * (self.force*self.force))
-            else :
-                F = -(0.5 * self.mass * (self.force*self.force))
+                F = (0.4 * self.mass * (self.force*self.force))
+            else:
+                F = -(0.4 * self.mass * (self.force*self.force))
 
             self.rect.centery = self.rect.centery - F
 
             self.force -= 1
 
-            if self.rect.centery >= 360:
-                self.rect.centery = 360
-                self.jumping = False
-                self.force = 8
+        if self.rect.centery >= 360:
+            self.rect.centery = 360
+            self.jumping = False
+            self.force = 8
 
 
-class Sakura(Character):
+class Maid(Character):
     def __init__(self, x, y):
         super().__init__(x, y)
 
     def get_image(self):
-        return pygame.image.load("./assets/sprites/SpriteSheets/Sakura/SakuraBasicModel.png").convert()
+        return pygame.image.load("./assets/sprites/SpriteSheets/Maid/MaidBasicModel.png").convert()
 
+
+class reDraw():
+    def redrawGameWindow(self, sprites):
+        sprites.draw(screen)
+
+        sprites.update()
+
+        pygame.display.flip()
+
+        screen.blit(background, (0, 0))
--- main_1.py
+++ main_1.py
@@ -1,42 +1,33 @@
 import pygame
-from fighting_game.helpers.screen import SCREEN_HEIGHT, SCREEN_WIDTH
-from fighting_game.characters import Sakura
-from fighting_game.helpers.colors import WHITE
+from fighting_game.characters import Maid, reDraw
 
 pygame.init()
 
-
-screen = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
-
 sprites = pygame.sprite.Group()
 
-sakura = Sakura(100, 360)
+maid = Maid(100, 360)
 
 
-sprites.add(sakura)
+sprites.add(maid)
 
 done = False
 
 clock = pygame.time.Clock()
 
-background = pygame.image.load('./assets/sprites/Backgrounds/BackgroundFairyTail.png').convert()
+reDraw = reDraw()
 
 while not done:
+    clock.tick(60)
+    key = pygame.key.get_pressed()
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
 
-    sprites.draw(screen)
 
     for sprite in sprites.sprites():
         sprite.handle_keydown()
 
-    sprites.update()
-
-    clock.tick(60)
-
-    pygame.display.flip()
-
-    screen.blit(background, (0, 0))
+    reDraw.redrawGameWindow(sprites)
 
 pygame.quit()
