f8128dc3f06e48344103f630a1e3de1962f649f1
Fabio <andresrenzas@gmail.com>
2019-02-25 17:23:20 -0500

    Encapsulado menu}

--- fighting_game/draw.py
+++ fighting_game/draw.py
@@ -50,63 +50,127 @@ virgo = pygame.transform.scale(virgo_load, (150, 200))
 
 # Transparent Surface
 transparentSurface = pygame.Surface((150,200))  # the size of your rect
-transparentSurface.set_alpha(128)                # alpha level
+transparentSurface.set_alpha(0)                # alpha level
 transparentSurface.fill((255,255,255))           # this fills the entire surface
 
 class redraw():
     character_menu = True
     first_menu = True
 
+    def print_character(self):
+
+        win.fill(black)
+
+        win.blit(bowsette, (50, 60))
+        win.blit(transparentSurface, (50, 60))
+
+        win.blit(maid, (50, 320))
+        win.blit(transparentSurface, (50, 320))
+
+        win.blit(miia, (240, 60))
+        win.blit(transparentSurface, (240, 60))
+
+        win.blit(ryuuko, (240, 320))
+        win.blit(transparentSurface, (240, 320))
+
+        win.blit(saber, (430, 60))
+        win.blit(transparentSurface, (430, 60))
+
+        win.blit(sailor, (430, 320))
+        win.blit(transparentSurface, (430, 320))
+
+        win.blit(sakura, (620, 60))
+        win.blit(transparentSurface, (620, 60))
+
+        win.blit(virgo, (620, 320))
+        win.blit(transparentSurface, (620, 320))
+
+    def main_menu(self):
+        mouse = pygame.mouse.get_pos()
+
+        win.blit(scaled_background_menu, (0, 0))
+
+        if 300 + 200 > mouse[0] > 300 and 250 + 50 > mouse[1] > 250:
+
+            pygame.draw.rect(win, red, (300, 250, 200, 50))
+
+            for event in pygame.event.get():
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    if event.button == 1:
+                        self.first_menu = False
+        else:
+            pygame.draw.rect(win, bright_red, (300, 250, 200, 50))
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+        win.blit(game_start, (300, 250))
+        pygame.display.flip()
+
+    def selection_character(self):
+        mouse = pygame.mouse.get_pos()
+        for event in pygame.event.get():
+
+            if 50 + 200 > mouse[0] > 50 and 60 + 150 > mouse[1] > 60:
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    if event.button == 1:
+                        print("Selected Bowsette")
+                        self.character_menu = False
+
+            elif 50 + 200 > mouse[0] > 50 and 320 + 150 > mouse[1] > 320:
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    if event.button == 1:
+                        print("Selected Maid")
+                        self.character_menu = False
+
+            elif 240 + 200 > mouse[0] > 240 and 60 + 150 > mouse[1] > 60:
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    if event.button == 1:
+                        print("Selected Miia")
+                        self.character_menu = False
+            elif 240 + 200 > mouse[0] > 240 and 320 + 150 > mouse[1] > 320:
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    if event.button == 1:
+                        print("Selected Ryuuko")
+                        self.character_menu = False
+
+            elif 430 + 200 > mouse[0] > 430 and 60 + 150 > mouse[1] > 60:
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    if event.button == 1:
+                        print("Selected Saber")
+                        self.character_menu = False
+
+            elif 430 + 200 > mouse[0] > 430 and 320 + 150 > mouse[1] > 320:
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    if event.button == 1:
+                        print("Selected Sailor")
+                        self.character_menu = False
+
+            elif 620 + 200 > mouse[0] > 620 and 60 + 150 > mouse[1] > 60:
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    if event.button == 1:
+                        print("Selected Sakura")
+                        self.character_menu = False
+
+            elif 620 + 200 > mouse[0] > 620 and 320 + 150 > mouse[1] > 320:
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    if event.button == 1:
+                        print("Selected Virgo")
+                        self.character_menu = False
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+        pygame.display.flip()
+
     def draw(self, sprites):
 
         if self.first_menu:
-
-            mouse = pygame.mouse.get_pos()
-
-            win.blit(scaled_background_menu, (0, 0))
-
-            if 300 + 200 > mouse[0] > 300 and 250 + 50 > mouse[1] > 250:
-
-                pygame.draw.rect(win, red, (300, 250, 200, 50))
-
-                for event in pygame.event.get():
-                    if event.type == pygame.MOUSEBUTTONDOWN:
-                        if event.button == 1:
-                            self.first_menu = False
-            else:
-                pygame.draw.rect(win, bright_red, (300, 250, 200, 50))
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    pygame.quit()
-                    quit()
-            win.blit(game_start, (300, 250))
-            pygame.display.flip()
+            self.main_menu()
 
         elif self.character_menu:
 
-            mouse = pygame.mouse.get_pos()
-
-            win.fill(black)
-
-            character_display = character_menu()
-            character_display.character_selection()
-
-            for event in pygame.event.get():
-                if 50 + 200 > mouse[0] > 50 and 60 + 150 > mouse[1] > 60:
-                    if event.type == pygame.MOUSEBUTTONDOWN:
-                        if event.button == 1:
-                            print("Selected Bowsette")
-                            self.character_menu = False
-                elif 50 + 200 > mouse[0] > 50 and 320 + 150 > mouse[1] > 320:
-                    if event.type == pygame.MOUSEBUTTONDOWN:
-                        if event.button == 1:
-                            print("Selected Maid")
-                            self.character_menu = False
-                if event.type == pygame.QUIT:
-                    pygame.quit()
-                    quit()
-            pygame.display.flip()
-
+            self.selection_character()
+            self.print_character()
 
         else:
             win.blit(scaled_background, (0, 0))
@@ -118,34 +182,3 @@ class redraw():
             pygame.display.flip()
 
             clock.tick(27)
-
-
-class character_menu():
-    def character_selection(self):
-
-        win.blit(bowsette, (50, 60))
-        win.blit(transparentSurface, (50, 60))
-
-        win.blit(maid, (50, 320))
-        win.blit(transparentSurface, (50, 320))
-
-        win.blit(miia, (240, 60))
-        win.blit(ryuuko, (240, 320))
-        win.blit(saber, (430, 60))
-        win.blit(sailor, (430, 320))
-        win.blit(sakura, (620, 60))
-        win.blit(virgo, (620, 320))
-
-class character_selection_rect():
-    mouse = pygame.mouse.get_pos()
-
-    for event in pygame.event.get():
-        if 50 + 200 > mouse[0] > 50 and 60 + 150 > mouse[1] > 60:
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if event.button == 1:
-                    print("Selected Bowsette")
-
-        if 50 + 200 > mouse[0] > 50 and 320 + 150 > mouse[1] > 320:
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if event.button == 1:
-                    print("Selected Maid")
